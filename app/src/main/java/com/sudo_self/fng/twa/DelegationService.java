1953534bd6ceeb764261707c2593bd1cad907b21
Sudo-Self <sudo-self@proton.me>
2023-12-19 17:25:22 -0700

    Add files via upload

---
+++ app/src/main/java/com/sudo_self/fng/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.sudo_self.fng.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
